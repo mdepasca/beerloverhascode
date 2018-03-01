b11ecbc1e5ae823e97787ef090ef1fae57112852
marco <marco@stellaplano.localdomain>
2018-03-01 21:33:28 +0100

    Add

---
+++ ddepa.py
@@ -0,0 +1,42 @@
+import numpy
+
+import numpy as np
+
+inFile = "a_example.in"
+h = np.genfromtxt(inFile, max_rows=1)
+d = np.genfromtxt(inFile, skip_header=1) 
+
+rows_tot = h[0]
+cols_tot = h[1]
+vehicles = h[2]
+rides = h[3]
+bonus = h[4]
+sim_time = h[5]
+
+
+class ride:
+    def __init__(self, startX, startY, endX, endY, earliestStart, latestFinish):
+        self.startX = startX
+        self.startY = startY
+        self.endX = endX
+        self.endY = endY
+        self.earliest_start = earliestStart
+        self.latest_finist = latestFinish
+        self.done = False
+
+    def vehicle_dist(vehicleX, vehicleY):
+        vehicleDist = abs(vehicleX-self.startX) + abs(vehicleY-self.startY)
+        return vehicleDist
+
+    def set_as_done():
+        self.done = True
+
+    def is_done():
+        return self.done
+
+    def vehicle_time(vehicleX, vehicleY):
+        # time to reach starting time is equal to distance from starting point
+        vehicleDist = abs(vehicleX-self.startX) + abs(vehicleY-self.startY)
+        return vehicleDist
+
+
